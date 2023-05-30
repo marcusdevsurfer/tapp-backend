888ae91188ea5cf88d017c34a5cb846153481c3e
marcusdev99 <marcus.sea13@gmail.com>
2023-05-30 17:23:27 -0600

    Creacion de Controlador "Transportista"

---
+++ src/main/java/com/devcoast/appbackend/controllers/CarrierController.java
@@ -0,0 +1,21 @@
+package com.devcoast.appbackend.controllers;
+
+import com.devcoast.appbackend.model.Carrier;
+import com.devcoast.appbackend.service.CarrierService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CarrierController {
+    @Autowired
+    CarrierService carrierService;
+    @GetMapping("/carriers")
+    public List<Carrier> retriveAllCarriers(){
+        return carrierService.getAllCarriers();
+
+    }
+}
