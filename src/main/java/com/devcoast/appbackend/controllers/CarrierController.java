8e6b704b67f5fa998f04ce9a113f33b5a92b4c73
marcusdev99 <marcus.sea13@gmail.com>
2023-05-31 19:23:38 -0600

    Modificacion CarrierController -Se agrego la notacion @CrossOrigin para poder consumir controlador con React.

--- src/main/java/com/devcoast/appbackend/controllers/CarrierController.java
+++ src/main/java/com/devcoast/appbackend/controllers/CarrierController.java
@@ -4,18 +4,19 @@ import com.devcoast.appbackend.model.Carrier;
 import com.devcoast.appbackend.service.CarrierService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class CarrierController {
     @Autowired
     CarrierService carrierService;
     @GetMapping("/carriers")
     public List<Carrier> retriveAllCarriers(){
         return carrierService.getAllCarriers();
-
     }
 }
